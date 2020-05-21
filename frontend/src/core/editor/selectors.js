95c72ab6e53ec129cc0f2942ba0e79fb311ad883
Adrian Gaudebert <adrian@gaudebert.fr>
2020-05-21 18:14:33 +0200

    Remove useless try/catch block that was put there for debugging.

--- frontend/src/core/editor/selectors.js
+++ frontend/src/core/editor/selectors.js
@@ -80,13 +80,7 @@ export function _existingTranslation(
                     fluentTranslation = fluent.getReconstructedMessage(entity.original, translation);
                 }
                 existingTranslation = history.translations.find(
-                    t => {
-                        try {
-                            fluentTranslation.equals(fluent.parser.parseEntry(t.string));
-                        } catch (e) {
-                        }
-                        return fluentTranslation.equals(fluent.parser.parseEntry(t.string));
-                    }
+                    t => fluentTranslation.equals(fluent.parser.parseEntry(t.string))
                 );
             }
             else {
